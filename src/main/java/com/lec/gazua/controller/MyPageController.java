a7d326786d8221d7eccf1fcb03ea7aca3d4f7e74
jjh5324 <jjh5324@kakao.com>
2020-12-08 14:39:01 +0900

    정호 : 커밋ㅅㅅ

--- src/main/java/com/lec/gazua/controller/MyPageController.java
+++ src/main/java/com/lec/gazua/controller/MyPageController.java
@@ -2,19 +2,20 @@ package com.lec.gazua.controller;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("MyPage")
+@Controller
+@RequestMapping("/mypage")
 public class MyPageController {
 
 	@Autowired
 	SqlSession sqlSession;
 	
 	@RequestMapping("/")
-	public String helloText() {
-		return "Hello MyPage";
+	public String mypage() {
+		
+		return "mypage/main";
 	}
 	
 }
